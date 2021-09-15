e36566de94a36c530a296282b6362c3d0a126110
Hamed-deris <87459507+Hamed-deris@users.noreply.github.com>
2021-09-15 04:49:44 -0700

    Description
    Add Description  about credentials

--- src/components/credentials.jsx
+++ src/components/credentials.jsx
@@ -1,11 +1,11 @@
 const me = {
   client_id:
-    "371111317575-fi1qadots41qmhabrlft15rd18vg8bor.apps.googleusercontent.com",
-  project_id: "test-318715",
+    "Your client id from google console developer ",
+  project_id: "Your project id from google console developer",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_secret: "0FhL4HQfVjnWEF5hIbwyK71u",
+  client_secret: "Your client secret from google console developer",
   redirect_uris: ["https://localhost:3000"],
   javascript_origins: ["https://localhost:3000"],
   scope: "https://www.googleapis.com/auth/drive",
